a9f3505aa50cd957b319518daeb9aa2fe97bfe55
G.B.Abadio <gilabadio@gmail.com>
2020-10-02 19:27:39 -0400

    finalized html text generator

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer');
 
-//const fs = require('fs');
+const fs = require('fs');
 
 
-//const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
 
 //const pageHTML = generatePage(name, github);
@@ -139,7 +139,13 @@ if (!portfolioData.projects) {
   })
 };
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  });

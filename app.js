a7c5b196ae3ff4eaddef51837412fee2e5cf037a
G.B.Abadio <gilabadio@gmail.com>
2020-10-02 20:56:28 -0400

    finaled code transfer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 
 const generatePage = require('./src/page-template');
@@ -9,11 +9,8 @@ const generatePage = require('./src/page-template');
 //const pageHTML = generatePage(name, github);
 
 
-//fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
+//fs.writeFile('./dist/index.html');
 
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 const promptUser = () => {
 
 return inquirer.prompt([
@@ -141,11 +138,18 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-     });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
